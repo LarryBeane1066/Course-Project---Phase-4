5f2a7ed0e2aa8b771f3a3f2c01058cc8e5e254d6
LarryBeane1066 <Lawrence.BeaneJR@my.strayer.edu>
2022-12-03 16:39:16 -0800

    updated code

--- Course_Project___Phase_4.py
+++ Course_Project___Phase_4.py
@@ -5,22 +5,19 @@ from datetime import datetime
 ################################################################################
 def CreateUsers():
     print('##### Create users, passwords, and roles #####')
-    ########## Open the file user.txt in append mode and assign to UserFile
-     
+    UserFile = open("Users.txt", "a+") 
     while True:
-        ########## Write the line of code that will call function GetUserName and assign the return value to username
-        
+        username = GetUserName()
         if (username.upper() == "END"):
             break
-        ########## Write the line of code that will call function GetUserPassword and assign the return value to userpwd      
-        ########## Write the line of code that will call function GetUserRole() and assign the return value to userrole
-        
+        userpwd = GetUserPassword()
+        userrole = GetUserRole()
+
         UserDetail = username + "|" + userpwd + "|" + userrole + "\n"  
         UserFile.write(UserDetail)
     # close file to save data
-    ########## Write the line of code that will close the file UserFile
     UserFile.close()    
-    
+    printuserinfo()
 
 def GetUserName():
     username = input("Enter user name or 'End' to quit: ")
@@ -55,20 +52,16 @@ def printuserinfo():
 
 def Login():
         # read login information and store in a list
-    ########## Write the line of code that will open the file Users.txt in read mode
     UserFile = open("Users.txt","r")
-    
+    UserList= []
     UserName = input("Enter User Name: ")
     UserRole = "None"
     while True:
-       ########## Write the line of code that will read a line from UserFile and assign it to UserDetail
-             
+       UserDetail = UserFile.readline()       
        if not UserDetail:
            return UserRole, UserName
-       ########## Write the line of code that will replace the carriage return in UserDetail
-       
-       ########## Write the line of code that will split UserDetail on the pipe delimiter (|) and assign it to UserList
-                  
+       UserDetail = UserDetail.replace("\n", "") #remove carriage return from end of line
+       UserList = UserDetail.split("|")           
        if UserName == UserList[0]:
             UserRole = UserList[2]  # user is valid, return role
             return UserRole, UserName
@@ -96,7 +89,7 @@ def CalcTaxAndNetPay(hours, hourlyrate, taxrate):
     netpay = grosspay - incometax
     return grosspay, incometax, netpay
 
-def jprintinfo(DetailsPrinted):
+def printinfo(DetailsPrinted):
     TotEmployees = 0
     TotHours = 0.00
     TotGrossPay = 0.00
@@ -160,22 +153,18 @@ def PrintTotals(EmpTotals):
 
 if __name__ == "__main__":
     ##################################################
-    ########## Write the line of code to call the method CreateUsers
-    
+    CreateUsers()
     print()
     print("##### Data Entry #####")
-    ########## Write the line of code to assign UserRole and UserName to the functin Login
-     
+    UserRole, UserName = Login() 
     DetailsPrinted = False  ###
     EmpTotals = {} ###
-    ########## Write the if statement that will check to see if UserRole is equal to NONE (NOTE: code will show red error lines until this line is written)
-    
+    if (UserRole.upper() == "NONE"): #user not found in user file
         print(UserName," is invalid.")
     else:
     # only admin users can enter data
-        ##### write the if statement that will check to see if the UserRole is equal to ADMIN (NOTE: code will show red error lines until this line is written)
-        
-   
+        if (UserRole.upper() == "ADMIN"):
+   ###############################################
             EmpFile = open("Employees.txt", "a+")                
             while True:
                 empname = GetEmpName()
@@ -190,3 +179,10 @@ if __name__ == "__main__":
         # close file to save data
             EmpFile.close()    
         printinfo(DetailsPrinted)
+
+
+
+
+
+
+dmi
